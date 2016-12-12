32e32a6cdb275e2007564fa68caaf7919318e180
Davy Zou <davy.zou@ericsson.com>
2016-12-12 11:11:38 -0500

    3

--- sendemail.py
+++ sendemail.py
@@ -2,13 +2,18 @@ import smtplib
 import base64
 from passlib.hash import pbkdf2_sha256
 
-TO = ''
+TO = 'jzoudavy@gmail.com'
 SUBJECT = 'TEST MAIL'
 TEXT = 'Here is a message from python.'
 
+#read the hidden file for the hashed password
+
+#from passlib.hash import pbkdf2_sha256
+#pbkdf2_sha256.verify("password", hash)
+#hash = pbkdf2_sha256.encrypt("password", rounds=200000, salt_size=16)
 
 # Gmail Sign In
-gmail_sender = ''
+gmail_sender = TO
 gmail_passwd = ''
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
